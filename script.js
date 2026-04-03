76f26578949d6ef309fcdc34992ce3a48750c04c
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-04-03 11:35:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const apkLink = "https://github.com/JAY01-CYBER/M3-Play/releases";
+document.getElementById("apk-link").setAttribute("href", apkLink);
